86488a5ea2364269614f156c35d5218fa21d9b26
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-10-24 19:55:22 +0700

    setup db

---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+using var scope = app.Services.CreateScope();
+var service = scope.ServiceProvider;
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    var context = service.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception Ex)
+{
+    var logger = service.GetRequiredService<ILogger<Program>>();
+    logger.LogError(Ex, "An error occured during migration");
+}
+
+app.Run();
